6b47e2139c3230c891820a1b222dec6f0590fd26
Debasish Biswas <debasishbsws.abc@gmail.com>
2022-12-22 11:35:22 +0000

    Adding docker files
    Signed-off-by: Debasish Biswas <debasishbsws.abc@gmail.com>

--- code/index.js
+++ code/index.js
@@ -1,13 +1,15 @@
 const express = require('express');
-const PORT = process.env.PORT || 3000;
 const app = express();
 
+//values from env
+const PORT = process.env.PORT || 3000;
+
+//request
 app.get('/', (req, res) => {
     res.send('<h1>Hello World!</h1>');
 });
 
-
-
+//app listening
 app.listen(PORT, () => {
     console.log(`App listening at http://localhost:${PORT}`)
 });
